da700db84850c56010c648bcfa625522d7672d81
wIn2_3z <quockhanh03.bg@gmail.com>
2023-02-17 01:49:21 +0700

    update Training_KCSC

--- Training_KCSC/#Task_1/#4_Simple-Addition/test.asm
+++ Training_KCSC/#Task_1/#4_Simple-Addition/test.asm
@@ -1,11 +1,11 @@
-section .bss 						;block starting symbol
-	str1 resb 33
-	num1 resb 10
-	str2 resb 33
-	num2 resb 10
-	sum resb 33
-	so_nho resb 2
-	big_num resb 10
+section .bss 						
+	str1 resb 33					;value1 
+	num1 resb 33					;len_value1
+	str2 resb 33					;value2
+	num2 resb 33					;len_value2
+	sum resb 33						;sum of 2 value
+	so_nho resb 2					;9+8 = 17 -> 1 is [so_nho]
+	big_num resb 33					;the bigger of len_value1 & len_value2 is big_num (len_of_value3)
 
 section .data
 	msg1: db "Nhap so thu nhat: "
@@ -17,82 +17,101 @@ section .data
 	msg4: db "Toi da 31 bit"
 	len4 equ $ - msg4
 
+;==============================================================================================================
+
 section .text
 	global _start
 _start:
-	mov ecx,msg1		;msg1
+	mov byte [str1], 0
+	mov byte [str2], 0
+	mov byte [sum], 0
+	mov ecx,msg1					;msg1
 	mov edx,len1
 	call printf   
 	
-	mov ecx,str1		;in and output str1
+	mov ecx,str1					;in and output str1
 	inc ecx
 	mov edx,33
 	call scanf
 	mov [num1], eax
 	cmp eax,31
-	ja too_big			;Bigger than 31bit then exit
+	ja too_big						;Bigger than 31bit then exit
 	
-	mov ecx,msg2		;msg2
+	mov ecx,msg2					;msg2
 	mov edx,len2
 	call printf
 	
-	mov ecx,str2		;in and output str2
+	mov ecx,str2					;in and output str2
 	mov edx,33
 	call scanf
 	mov [num2], eax
 	mov [big_num], eax
-	dec byte [big_num]
 	cmp eax,31
-	ja too_big			;Bigger than 31bit then exit
+	ja too_big						;Bigger than 31bit then exit
 	
 	mov eax, [num1]
 	mov ebx, [num2]
-	cmp eax, ebx
-	ja str1_big 		;Save the bigger value of len_str1 & len_str2  to big_num	
-		
+	cmp eax, ebx		
+	
+	ja str1_big 					;Save the bigger value of len_str1 & len_str2  to big_num	
+	call addition_ok 					;Start of calculation
+
 done:
-	mov ecx,msg3		;print msg3
+	mov ecx,msg3					;print msg3
 	mov edx,len3
 	call printf
 	
-	mov ecx,sum			;print sum
+	mov edi, sum					; 3+4 = 07 -> set 0 to null
+	inc edi
+	mov al, [edi]
+	cmp al, '0'
+	jne hehe
+	sub al,'0'
+	mov [edi], al
+hehe:
+	mov ecx,sum						;print sum
 	add ecx,1
 	mov edx,33
 	call printf
 	
 	call exit
-;============================================================================
-addition_ok:
-	mov eax, str1
+;==============================================================================================================
+
+;functions go here
+						
+str1_big:							;begin big_num = num2 and this make big_num = num1
+	mov byte [big_num], 0	
+	mov [big_num], eax
+
+addition_ok:						;consider 2 numbers in the same order 
+	mov eax, str1			
 	mov ebx, str2
 	add eax, [num1]
 	dec eax
 	add ebx, [num2]
-	sub ebx, 2
-	call addition
-	
-addition:
+	sub ebx, 2	
+addition:							;for ( ; big_num>0; big_num--)
 	mov edi, sum
 	add edi, [big_num]
 	mov cl, [eax]
 	cmp cl, 0
-	je addcl
+	je addcl						;if [num_1+n] = null -> add '0' for the sub '0' behind
+con1:
 	mov dl, [ebx]
 	cmp dl, 0
 	jne con
-	add dl, '0'
-
-con:	
+	add dl, '0'						;if [num_2+n] = null -> add '0' for the sub '0' behind
+con:			
 	sub cl, '0'
 	sub dl, '0'
-	add cl, dl			;cong 2 so luu vao cl
-	add cl, [so_nho]		;cong them phan nho 1 hoac 0 vao cl
+	add cl, dl						;sum 2 value and add to cl
+	add cl, [so_nho]					;add [so_nho] to cl
 	
 	mov byte [so_nho], 0
 	cmp cl, 9
-	ja above9
-	
-save:
+	ja above9						;if a = 11 > 9 -> a sub 10 = 1 and [so_nho] = 1
+		
+save:								;save cl to edi = sum and dec the addr of value1; value2; sum
 	add cl, '0'
 	mov [edi], cl
 	dec eax
@@ -101,19 +120,14 @@ save:
 	sub byte [big_num], 1
 	mov ebp, [big_num]
 	mov [big_num], ebp
-	cmp ebp, 1
-	jz done
+	cmp ebp, 0
+	je done						;if [big_num] = 0 then end the program (loop [big_num] times)
 	call addition
 
 above9:	
-	sub cl, 0xa
+	sub cl, 10
 	mov byte [so_nho], 1
 	call save
-
-str1_big:
-	mov byte [big_num], 0
-	mov [big_num], eax
-	call addition_ok
 
 too_big:
 	mov ecx, msg4
@@ -121,7 +135,6 @@ too_big:
 	call printf
 	
 	call exit
-
 
 scanf: 
 	mov eax,3
@@ -141,7 +154,7 @@ exit:
 
 addcl: 
 	add cl, '0'
-	jmp con
+	jmp con1
 
 
 
