3624a7c2d9190687791005abbbdd0c2fa96c3838
F4t_c4t <97304629+wIn2-3z@users.noreply.github.com>
2023-12-11 01:26:38 +0700

    Update_WU

---
+++ Competition/ACS_2023/web1/chall.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+echo "<h2>Do you need phpinfo? ... or not?</h2>";
+
+$num=$_GET['num'];
+$page=$_GET['page'];
+
+if(preg_match("/^[0-9+-\/\*e ]/i", $num)){
+    exit("<h2>I hate number<h2>");
+}
+
+if(preg_match("/flag|\.|php|conf|\*|'|\"/i", $page)){
+    exit("<h2>don't do that.</h2>");
+}
+
+
+if(!is_numeric($num)){
+    echo('Bypassed numeric');
+    if($page==null){
+        echo phpinfo();
+    }else{
+        include_once($page);
+    }
+}else{
+    highlight_file(__FILE__);
+}
+
+?>
